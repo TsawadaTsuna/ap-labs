a0bc27d17db961a3aff81e88228862b5db6f1096
Kevin C <kevin.dany.contreras@gmail.com>
2021-08-16 19:25:52 +0000

    Solved test-lab

--- labs/test-lab/test-lab.go
+++ labs/test-lab/test-lab.go
@@ -2,8 +2,19 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Welcome to the jungle")
+	args := len(os.Args)
+	if args < 2 {
+		fmt.Println("No name provided, pls try again")
+	} else {
+		name := os.Args[1]
+		for i := 2; i < args; i++ {
+			name += " "+os.Args[i]
+		}
+		fmt.Printf("Hello %v Welcome to the GO \n", name)
+	}
+
 }

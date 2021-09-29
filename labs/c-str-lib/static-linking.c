5b9aebb0448a9d62640209d3be2a76e46c64a719
Kevin C <kevin.dany.contreras@gmail.com>
2021-09-29 17:47:57 +0000

    first done

--- labs/c-str-lib/static-linking.c
+++ labs/c-str-lib/static-linking.c
@@ -1,5 +1,30 @@
 #include <stdio.h>
 
-int main() {
+int mystrlen(char *);
+char *mystradd(char *, char *);
+int mystrfind(char *, char *);
+int mystrcmp(char *, char *);
+
+int main(int argc, char **argv) {
+    if (argc < 4){
+        printf("Error. Missing arguments\n");
+        printf("Pleas run as \n./main -add <string 1> <string 2>\nor\n./main -find <string 1> <string 2>\n");
+        return -1;
+    }
+    if (mystrcmp(argv[1],"-add")==0){
+        char *str = mystradd(argv[2],argv[3]);
+        printf("Initial Lenght      : %d\nNew String          : %s\nNew length          : %d\n",mystrlen(argv[2]),str,mystrlen(str));
+    }else if (mystrcmp(argv[1],"-find")==0){
+        int pos = mystrfind(argv[2],argv[3]);
+        if(pos!=-1){
+            printf("['%s'] string was found at [%d] position\n",argv[3],pos);
+        }else{
+            printf("['%s'] string was not found at ['%s'] string\n",argv[3],argv[2]);
+            return -1;
+        }
+    }else{
+        printf("%s is not a valid functionality\n",argv[1]);
+        return -1;
+    }
     return 0;
 }
--- labs/c-str-lib/strlib.c
+++ labs/c-str-lib/strlib.c
@@ -1,13 +1,70 @@
-
+#include <stdlib.h>
 
 int mystrlen(char *str){
-    return 0;
+    int c=0;
+    int act=0;
+    while (str[act]!=0){
+        c++;
+        act++;
+    }
+    return c;
 }
 
 char *mystradd(char *origin, char *addition){
-    return 0;
+    int lenOr = mystrlen(origin);
+    int lenAdd = mystrlen(addition);
+    char *newString = calloc(lenAdd+lenOr,sizeof(char));
+    int act=0;
+    while (act<lenOr){
+        newString[act] = origin[act];
+        act++;
+    }
+    int actAdd = 0;
+    while (actAdd<lenAdd){
+        newString[act]=addition[actAdd];
+        actAdd++;
+        act++;
+    }
+    return newString;
 }
 
 int mystrfind(char *origin, char *substr){
+    int lensub = mystrlen(substr);
+    int lentocheck = mystrlen(origin)-lensub;
+    int act = 0;
+    while (act<lentocheck){
+        if (substr[0]==origin[act])
+        {
+            int dif=0;//0 false 1 true
+            int j=act+1;
+            for (int i = 1; i < lensub; i++)
+            {
+                if(substr[i]!=origin[j]){
+                    i=lensub;
+                    dif = 1;
+                }else{
+                    j++;
+                }
+                
+            }
+            if(dif==0){
+                return act;
+            }
+        }
+        act++;
+    }
+    return -1;
+}
+
+int mystrcmp(char *string1, char *string2){
+    int len1=mystrlen(string1);
+    if (mystrlen(string2)!=len1){
+        return 1;
+    }
+    for (int i = 0; i < len1; i++){
+        if (string1[i]!=string2[i]){
+            return 1;
+        }
+    }
     return 0;
 }
